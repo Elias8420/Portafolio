8125ed815a011f8503bef3e0edf16a0ea1e2fcd5
Elias8420 <eliaszelaya747@gmail.com>
2024-08-19 02:33:34 -0600

    adding buildscript to use dagger

--- SimpleMessages/Frontend/app/build.gradle.kts
+++ SimpleMessages/Frontend/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
     id("kotlin-kapt")
+    //To use dagger
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
